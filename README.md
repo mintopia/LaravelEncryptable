cff04bee601ec7a612b3630b24da52585e343a99
Chris Stretton <chris.stretton@fasthosts.com>
2017-11-06 09:54:16 +0000

    Initial commit of library

---
+++ README.md
@@ -0,0 +1,46 @@
+# Laravel Encrypter Trait
+
+Provides a trait to decrypt or encrypt values in a Laravel model.
+
+## Usage
+
+Add the trait to your model, define the ```$encrypted``` array and fields in it will be encrypted and decrypted on the fly by default.
+
+```php
+namespace App;
+
+use Cheezykins\LaravelEncryptable\Traits\Encryptable;
+use Illuminate\Database\Eloquent\Model;
+
+class MyModel extends Model {
+    use Encryptable;
+
+    protected $encrypted = [
+        'name',
+        'email'
+    ];
+}
+```
+
+Then in usage:
+
+```php
+$model = new MyModel();
+$model->name = 'Chris';
+$model->email = 'chris@test.com';
+$model->save();
+```
+
+The name field in the database will now be the encrypted value.
+
+```eyJpdiI6IkRZS0lOUlwvR29MbU4zN1diYzl2ZCtnPT0iLCJ2YWx1ZSI6IldEYzVUajlUcDdvVHE0M0kxdForNlE9PSIsIm1hYyI6ImY1MzQ2ZWYwNTNkZDI2YTY2MDgyMmVjZmU3MmI0MGU0NTNmMmU4NWE4OGFmYzZhYTJlYzczMWU1YTdmNzNjYjQifQ==```
+
+**WARNING**
+
+The encrypted value is stored based on your Laravel APP_KEY using the algorithm defined in your config/app.php cipher setting. If your application key is changed or lost there is no way to retrieve the data.
+
+## Installation
+
+require the project and use it. There is no service provider as it is not needed.
+
+```composer require cheezykins/laravel-encryptable```
---
+++ src/Traits/Encryptable.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Cheezykins\LaravelEncryptable\Traits;
+
+
+use Illuminate\Support\Facades\Crypt;
+
+trait Encryptable
+{
+    protected $encrypted = [];
+
+    /**
+     * Decrypt the column value if it is in the encrypted array.
+     *
+     * @param $key
+     *
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        $value = parent::getAttribute($key);
+        if (in_array($key, $this->encrypted)) {
+            $value = Crypt::decrypt($value);
+        }
+        return $value;
+    }
+
+    /**
+     * Set the value, encrypting it if it is in the encrypted array.
+     *
+     * @param $key
+     *
+     * @param $value
+     */
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encrypted)) {
+            $value = Crypt::encrypt($value);
+        }
+        return parent::setAttribute($key, $value);
+    }
+
+    /**
+     * Retrieves all values and decrypts them if needed.
+     *
+     * @return mixed
+     */
+    public function attributesToArray()
+    {
+        $attributes = parent::attributesToArray();
+
+        foreach ($this->encrypted as $key) {
+            if (isset($attributes[$key])) {
+                $attributes[$key] = Crypt::decrypt($attributes[$key]);
+            }
+        }
+
+        return $attributes;
+    }
+}
